d9844d50043f0010171cea572d1d86056e7a236e
DESKTOP-A1C2M9S\Dvir <davidi.dvir@gmail.com>
2021-07-18 22:26:51 +0300

    Factory & Singleton home work

---
+++ HM1.js
@@ -0,0 +1,91 @@
+//Factory
+
+// A constructor for defining new toysDuck
+function ToyDuck(options) {
+    // some defaults
+    this.color = options.color;
+    this.price = options.price;
+}
+
+// A constructor for defining new toysCar
+function ToyCar(options) {
+    this.color = options.color;
+    this.price = options.price;
+    this.name = options.name;
+}
+
+// Define a skeleton toy factory
+//function ToyFactory() { 
+
+// Our default toyClass is Duck
+//this.toy = ToyDuck;
+//}
+
+// Define a skeleton toy factory
+var Singleton = (function ToyFactory() {
+    // Our default toyClass is Duck
+    this.toy = ToyDuck;
+    var instance;
+
+    function createInstance() {
+        var object = new Object("I am the instance");
+        return object;
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+// Our Factory method for creating new Toy instances
+ToyFactory.prototype.createToy = function (options) {
+    switch (options.toyType) {
+        case "car":
+            this.toy = ToyCar;
+            break;
+        case "duck":
+            this.toy = ToyDuck;
+            break;
+        //defaults to ToyFactory.prototype.toyClass (Toy)
+    }
+
+    return new this.toy(options);
+};
+
+function testFactory() {
+    // Create an instance of our factory that makes toys
+    var toyFactory = new ToyFactory();
+    var toy = toyFactory.createToy({
+        toyType: "car",
+        color: "yellow",
+        price: "100",
+        name: "Suzuki"
+    });
+
+    // Test to confirm our toy was created using the toyClass/prototype Toy
+
+    // Outputs: true
+    console.log(toy instanceof ToyCar);
+    console.log(toy instanceof ToyDuck);
+
+    // Outputs: Toy object of color "yellow", price: 100, name: Suzuki.
+    console.log(toy);
+   
+}
+
+function testSingleton() {
+    var instance1 = Singleton.getInstance();
+    var instance2 = Singleton.getInstance();
+    console.log("Same instance? " + (instance1 === instance2));
+}
+
+
+testFactory();
+testSingleton();
+
+
